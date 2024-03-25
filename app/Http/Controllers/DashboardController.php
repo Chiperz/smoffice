0076d461f67bab8260bef21a232f83a329b058fc
Afif Al Fakri <afif.al.fakri@gmail.com>
2024-03-25 16:35:32 +0700

    Completing Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
+use App\Models\User;
+use App\Models\HeaderVisit;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard');
+        $store = Customer::where('type', 'S')->get()->count();
+        $outlet = Customer::where('type', 'O')->get()->count();
+        $user = User::all()->count();
+        $visit = HeaderVisit::where('user_id', Auth::user()->id)
+            ->where('date', date('Y-m-d'))->get()->count();
+        return view('dashboard', compact('store', 'outlet', 'user', 'visit'));
     }
 }
