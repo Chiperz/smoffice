5fd7951b87513ca1e701e6b2d3f1b35a7e1b69e6
Afif Al Fakri <afif.al.fakri@gmail.com>
2024-05-27 10:09:20 +0700

    Fix issue in export store and outlet

--- app/Exports/OutletExport.php
+++ app/Exports/OutletExport.php
@@ -5,17 +5,32 @@ namespace App\Exports;
 use App\Models\Customer;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class OutletExport implements FromCollection, WithHeadings
+class OutletExport implements FromCollection ,WithMapping, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
+    
     public function collection()
     {
-        return Customer::select('code', 'name', 'phone', 'address', 'LA', 'LO', 'area', 'subarea')
-        ->where('type', 'O')
-        ->get();
+        return Customer::where('type', 'O')->get();
+    }
+
+    public function map($store): array
+    {
+        return [
+            $store->code,
+            $store->name,
+            $store->phone,
+            $store->address,
+            $store->LA,
+            $store->LO,
+            empty($store->deploy_area) ? '' : $store->deploy_area->name,
+            empty($store->deploy_sub_area) ? '' : $store->deploy_sub_area->name,
+            empty($store->deploy_branch) ? '' : $store->deploy_branch->name,
+        ];
     }
 
     public function headings(): array{
@@ -27,7 +42,8 @@ class OutletExport implements FromCollection, WithHeadings
             'Latitude',
             'Longitude',
             'Area',
-            'Sub Area'
+            'Sub Area',
+            'Cabang'
         ];
     }
 }
